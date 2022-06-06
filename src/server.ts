887b4a0cf48baaef4e0f59be551e28717ac9152d
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-06 16:28:47 -0300

    test: learning typescript libs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+app.get("/test", (req, res) => {
+  return res.send("Olá NLW!");
+});
+
+app.post("/test-post", (req, res) => {
+  return res.send("Olá NLW método POST!");
+});
 
 app.listen(3000, () => console.log("Server is running!"));
