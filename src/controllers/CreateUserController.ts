83b2b3fd648fae2ddff5163272b162cb48ad4a12
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-09 17:04:29 -0300

    feat: bcrypt hash password created

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../entities/User";
 import { AppDataSource } from "../database/index";
+import { hash } from "bcryptjs";
 
 export class CreateUserController {
   async handle(request: Request, response: Response, next: NextFunction) {
     const userRepository = AppDataSource.getRepository(User);
     const { name, email, password, admin } = request.body;
+    const passwordHash = await hash(password, 10);
     if (!email) {
       throw new Error("Incorrect email!");
     }
@@ -17,7 +19,7 @@ export class CreateUserController {
       name,
       email,
       admin,
-      password,
+      password: passwordHash,
     });
     await userRepository.save(user);
     return response.status(200).json(user);
