887e6a349c9fc3614b699c2a8f58a9d4ccabb5ed
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-10 11:15:04 -0300

    feat: class transformer to hide password and change name data

--- src/database/entities/Tag.ts
+++ src/database/entities/Tag.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
-
+import { Expose } from "class-transformer";
 @Entity("tags")
 export class Tag {
   @PrimaryColumn()
@@ -15,6 +15,10 @@ export class Tag {
   @UpdateDateColumn()
   updated_at: Date;
 
+  @Expose({ name: "customName" })
+  nameCustom(): string {
+    return `#${this.name}`;
+  }
   constructor() {
     if (!this.id) {
       this.id = uuid();
