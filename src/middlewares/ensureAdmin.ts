8228041bb319013e85ecb462ab46ab0aa9acbae4
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-10 10:25:34 -0300

    refactor: updating admin check to create tags

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,7 +1,12 @@
 import { Request, Response, NextFunction } from "express";
+import { AppDataSource } from "../database";
+import { User } from "../database/entities/User";
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-  const admin = true;
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+  const { user_id } = request;
+  console.log(user_id);
+  const usersRepository = AppDataSource.getRepository(User);
+  const { admin } = await usersRepository.findOneBy({ id: user_id });
   if (admin) {
     return next();
   }
